c4a83cc8713f9bcc41f467a359e9d05805334f45
“Dominik <s20577@pjwstk.edu.pl>
2019-10-10 12:57:24 +0200

    added sum function

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ int main() {
     int liczba = 55;
     int user_input;
     cout << "Hello, World!";
+    int a;
+    int b;
+    wynik = a + b;
     cout << "Podaje liczbe";
-    cout << "I tak nie zdam";
+    cout << wynik << "I tak nie zdam";
     return 0;
 }
