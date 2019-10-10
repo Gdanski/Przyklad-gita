c29b9d59e08995683fa6e0cfee62a956ebefde3b
“Dominik <s20577@pjwstk.edu.pl>
2019-10-10 12:56:19 +0200

    added math.h

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
+#include <math.h>
 int main() {
     int liczba = 55;
     int user_input;
     cout << "Hello, World!";
     cout << "Podaje liczbe";
+    cout << "I tak nie zdam";
     return 0;
 }
